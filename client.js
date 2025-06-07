dc8b3f2c186885225e2736d26c4cbc7b3174e7e0
ztigma <48769371+ztigma@users.noreply.github.com>
2025-06-07 17:37:41 -0400

    Update client.js

--- client.js
+++ client.js
@@ -119,32 +119,5 @@ window.addEventListener('load', function() {
 });
 function OnStart ()
 {
-    var w = new Vista("div");
-    var boton = new Botonsito();
 
-    w
-    .set_attrs
-    ({
-        "_orientation":"horizontal_center"
-    })
-    .set_styles
-    ({
-        "color":"blue"
-    })
-    .set_childrens
-    ([
-        "SOY UN DIV"
-    ])
-
-    boton
-    .set_childrens
-    ([
-        "soy un botonsito"
-    ])
-    document.body.set_childrens
-    ([
-        w
-        ,
-        boton
-    ])
 }
